5c7206e9ed8c3bbe9b28b0af9926f9f3ab6ae665
Miguel Angel Sanchez <miguesanchez82003@gmail.com>
2024-05-13 17:11:18 -0500

    Boton de cargar

--- Escenas/mundo.gd
+++ Escenas/mundo.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
+var SAVE_PATH = "user://variable.save"
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://mundo.tscn")
-	pass # Replace with function body.
 
 func _on_button_3_pressed():
 	get_tree().quit()
-	pass # Replace with function body.
+
+func _on_cargar_partida_ready():
+	var button = $"VBoxContainer/Cargar Partida"
+	if FileAccess.file_exists(SAVE_PATH):
+		pass
+	else:
+		button.visible = false
